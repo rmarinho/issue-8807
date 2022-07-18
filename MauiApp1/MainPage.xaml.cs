a92703e88245a18e5113fc81a8cb65dbdbbddf7a
Rui Marinho <me@ruimarinho.net>
2022-07-18 17:10:07 +0100

    Add repo for issue #8807

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
+public class SampleView : GraphicsView
+{
+    public SampleView()
+    {
+        Drawable = new EllipseDrawable();
+    }
+
+    protected override Size MeasureOverride(double widthConstraint, double heightConstraint)
+    {
+        return new Size(widthConstraint, heightConstraint);
+    }
+}
+
+public class EllipseDrawable : IDrawable
+{
+    public void Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        Rect ellipseBounds = new Rect(dirtyRect.Center, new Size(30));
+        canvas.FillColor = Colors.Blue;
+        canvas.SetShadow(new SizeF(0f, 10f), 10f, Colors.Red);
+        canvas.DrawString("Welcome to MAUI !!!", 10, 50, HorizontalAlignment.Left);
+        canvas.FillEllipse(ellipseBounds);
+    }
+}
+
